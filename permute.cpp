3286af3fc2273d83853501f76ec07cae8c9c2f30
Abhishek Shrivastav <abhishek.shrivastav24@gmail.com>
2016-07-02 23:21:28 +0530

    Fixed permute

--- permute.cpp
+++ permute.cpp
@@ -1,21 +1,33 @@
 #include <iostream>
-
+#include <algorithm>
 
 
 using namespace std;
 
-void permute(int arr[], int b)
+void permute(auto str, auto start, auto end)
+
 {
 
-
+    if (start == end)
+    {
+        for(auto &i : str)
+        cout << i;
+        cout << endl;
     }
 
-
-
+   for(auto i = start; i < end ; i++)
+   {
+       char temp;
+       temp = str[i]; str[i] = str[start]; str[start]=temp;
+       permute(str, start+1, end);
+       temp = str[i]; str[i] = str[start]; str[start]=temp;
+    }
+}
 int main()
 {
-    int arr[5] = {1,2,3,4,5};
-    permute(arr, 5);
+    vector<char> str = {'a','b','c','d'};
+    auto size = str.size();
+    permute(str, 0,size);
 
 
 }
