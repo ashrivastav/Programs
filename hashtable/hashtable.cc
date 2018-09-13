3eb52ab6b57d1a60eda39eb2275f29ee3357ab33
Abhishek Shrivastav <abhishek.shrivastav24@gmail.com>
2018-09-14 02:43:39 +0530

    Concurrent HashTable with more enhancements

--- hashtable/hashtable.cc
+++ hashtable/hashtable.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <list>
 #include <vector>
+#include <shared_mutex>
 #include <mutex>
 #include <thread>
 #include <cassert>
@@ -8,41 +9,47 @@
 #include <thread>
 
 using namespace std;
+const size_t BUCKET_SIZE = 1024;
 
-
-/* Assumption, key will be unique
-   Key will be inserted only once
-   as Lookup will be done on key and then insertion
-   will happen */
 template<typename K, typename V>
-class HashTable {
-  
-  class ListEntry {
+/* Entry for the HashTable */
+class HashEntry {
   public:
     K key_;
     V value_;
-    ListEntry(K key, V value):key_(key), value_(value){}
-  };
+    HashEntry(K key, V value):key_(key), value_(value){}
+};
 
-  class HashEntry {
-  public:
-    size_t numElem_ = 0;
-    mutex mut_;
-    list<ListEntry> bucket_;
-    bool Insert(const ListEntry& entry)
+/* Buckets for HashTable, containing HashEntry vector(or list) */
+template<typename K, typename V>
+class HashBucket {
+  private:
+    size_t numBucketElem_ = 0;
+    mutable shared_mutex mut_; // Reader writer lock
+                                    // for cache locality which is fast
+
+ public:
+    vector<HashEntry<K,V> >bucket_; //Using vector in place of list 
+    // Needed in HashTable for iterating over, for resize
+    void Insert(const HashEntry<K,V>& entry)
     {
-      auto key = entry.key_;
-      for (auto& it : bucket_) {
-        if (it.key_ == key)
-          return false;
-      }
+      unique_lock<shared_mutex> locker(mut_);
+        auto key = entry.key_;
+        for (auto& it : bucket_) {
+          if (it.key_ == key) {
+            it.value_ = entry.value_;
+            goto done;
+          }
+        }
       bucket_.push_back(entry);
-      numElem_++;
-      return true;
+    done:
+      numBucketElem_++;
+      return;
     }
 
     bool Find(const K &key, V &value)
     {
+      unique_lock<shared_mutex> locker(mut_);
       for (auto& it : bucket_) {
         if (it.key_ == key) {
           value = it.value_;
@@ -51,38 +58,61 @@ class HashTable {
       }
       return false;
     }
-  };
-  
-  const size_t BUCKET_SIZE = 128;
 
-  size_t  tableSize_ = 0;
-  size_t  loadFactor_ = 0;
-  vector<HashEntry> table_;
+    void Remove(const K& key)
+    {
+      unique_lock<shared_mutex> locker(mut_);
+      for(auto it = bucket_.begin(); it != bucket_.end() ; ) {
+        if (it->key_ == key)
+          bucket_.erase(it);
+      }
+    }
+    
+    void Clear()
+    {
+      unique_lock<shared_mutex> locker(mut_);
+      // for vector
+      bucket_.clear();
+      bucket_.shrink_to_fit();
+    }
+};
  
+/* HashTable, containing HashBucket */
+//Key, Value, HashFunction(default- std::hash)
+template<typename K, typename V, typename F = hash<K>>
+class HashTable {
+public:
+  size_t  tableSize_ = 0;
+  double  numElem_ = 1;
+  double  loadFactor_ = 0;
+  HashBucket<K,V> *table_; 
+  //Can't use vector for table_ as HashBucket has mutex, which is non movable 
+  //and doesn't provide copy constructor, so resize will not work as it needs
+  //to copy all values in new vector space if In case when we increase tableSize_
+ 
+  HashTable(const size_t tableSize = BUCKET_SIZE) {
+    tableSize_ = tableSize;
+    table_  =  new HashBucket<K,V> [tableSize];
+  }
+
   size_t MyHash(K key) 
   {
-    hash<K> hasher;
+    F hasher;
     return hasher(key) % tableSize_;
   }   
 
-public:
-
-  HashTable() {
-    HashTable(BUCKET_SIZE);
-  }
-
-  HashTable(const size_t tableSize) {
-    tableSize_ = tableSize;
-    table_.resize(tableSize);
-    cout << table_.size();
-    this_thread::sleep_for(chrono::seconds(10));
-  }
-
-  bool Put(K key, V value) 
+  void Put(K key, V value) 
   {
     size_t mykey = MyHash(key);
-    ListEntry entry(key, value);
-    return table_[mykey].Insert(entry);
+    HashEntry<K,V> entry(key, value);
+    table_[mykey].Insert(entry);
+    numElem_++;
+    loadFactor_ = numElem_ / tableSize_;
+    cout << loadFactor_ << endl;
+    if (loadFactor_ > 0.75) {
+      ReSize(); 
+    }
+    return;
         
   }
 
@@ -92,28 +122,55 @@ public:
     return table_[mykey].Find(key, value); 
   }
 
+  void Delete(const K& key)
+  {
+    size_t mykey = MyHash(key);
+    table_[mykey].Remove(key);
+  }
 
+  void Resize()
+  {
+    size_t oldTableSize = tableSize_;
+    auto oldTableRef = table_;
+
+    tableSize_ = 2 * tableSize_; //double each time
+    table_  =  new HashBucket<K,V> [tableSize_];
+    for (int i = 0 ; i < oldTableSize; i++) {
+      oldTableRef[i].;
+      oldTableRef[i].Clear();
+    }
+    delete[] table_;
+  }
+
+  ~HashTable() {
+   for (int i = 0 ; i < tableSize_; i++) {
+      table_[i].Clear();
+    }
+    delete[] table_;
+  }
 
 };
-
-
 
 int main()
 {
   HashTable<string, int> hashTable;
-  /*bool result;
-  result = hashTable.Put("abhishek", 8);
-  assert(result);
-  result = hashTable.Put("abhi", 4);
-  assert(result);
-  result = hashTable.Put("abhis", 5);
-  assert(result);
-  result = hashTable.Put("abh", 3);
-  assert(result);
+  hashTable.Put("abhishek", 8);
+  hashTable.Put("abhi", 4);
+  hashTable.Put("abhis", 5);
+  hashTable.Put("abh", 3);
   int value;
+  bool result = hashTable.Get("abhis", value);
+  if (result)
+    cout << "key abhis, Value:" << value << endl;
+  else
+    cout << "Not found";
+  
+  hashTable.Delete("abhis");
   result = hashTable.Get("abhis", value);
   if (result)
-    cout << "key abhis, Value:" << value;
-    */
+    cout << "key abhis, Value:" << value << endl;
+  else
+    cout << "Not found";
+   
 }
 
