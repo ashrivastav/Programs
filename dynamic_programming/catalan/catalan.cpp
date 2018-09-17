6f4316f12bcef97dc153ea4fbad34ccec1b0f7ef
Abhishek Shrivastav <abhishek.shrivastav24@gmail.com>
2018-09-18 01:31:25 +0530

    Tested Catalan Number using Dynamic Programming

--- dynamic_programming/catalan/catalan.cpp
+++ dynamic_programming/catalan/catalan.cpp
@@ -10,16 +10,16 @@ using namespace std;
 long long ComputeCatalan(long long numKeys)
 {
   if (numKeys < 1)
-    return 1;
+    return 1; 
 
-    long long count = 0;
-    for (long long i=1; i <= numKeys ; i++ ) {
-      count += ComputeCatalan(numKeys - i) * ComputeCatalan(i -1 );
-    }
-    return count;
+  long long count = 0;
+  for (long long i=1; i <= numKeys ; i++ ) {
+    count += ComputeCatalan(numKeys - i) * ComputeCatalan(i -1 );
+  }
+  return count;
 }
 
-// Memoization
+// Dynamic Programming, bottom up
 long long ComputeCatalanDynamic(long long numKeys)
 {
   vector<long long> catalanNum;
@@ -27,10 +27,9 @@ long long ComputeCatalanDynamic(long long numKeys)
   catalanNum[0] = 1; // Null tree, needed for recursion
   catalanNum[1] = 1; // Null tree, needed for recursion
   
-  int count = 0;
   for(int i = 2; i <= numKeys; i++)
   {
-    catalaNum[i] = 0;
+    catalanNum[i] = 0;
     for( int j = 0 ; j < i; j++ ) // Use lower
     {
       catalanNum[i] += catalanNum[j] * catalanNum [i - j - 1];
@@ -40,30 +39,8 @@ long long ComputeCatalanDynamic(long long numKeys)
   return catalanNum[numKeys];
 }
 
-// Memoization
-long long ComputeCatalanDynamic1(long long numKeys)
-{
-  catalanNum[0] = 1; // Null tree, needed for recursion
-  catalanNum[1] = 1; // Null tree, needed for recursion
-  
-  if catalaNum
-  {
-    catalaNum[i] = 0;
-    for( int j = 1 ; j <= numkeys; j++ ) // Use lower
-    {
-      catalanNum[i] += catalanNum[j] * catalanNum [i - j - 1];
-    }
-  }
-
-  return catalanNum[numKeys];
-}
-
-
 int main()
 {
-  vector<long long> catalanNum;
-  catalanNum.reserve(5000);
-  catalanNum.fill();
   long long numKeys = 0 ;
   cin >> numKeys;
   cout << "Catalan Number" << ComputeCatalanDynamic(numKeys) << endl;  
